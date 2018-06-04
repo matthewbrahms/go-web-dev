35da27167a6955e7a4514d4208de2d2180ede66f
Matthew Brahms <matthewbrahms@gmail.com>
2018-06-03 21:41:51 -0500

    Finished introductory code review

--- 00-language-review/func-literals/main.go
+++ 00-language-review/func-literals/main.go
@@ -2,6 +2,62 @@ package main
 
 import "fmt"
 
+type person struct {
+	firstName string
+	lastName  string
+}
+
+type secretAgent struct {
+	person
+	licenseToKill bool
+}
+
+type human interface {
+	speak()
+}
+
+func (p person) speak() {
+	fmt.Println(p.firstName, p.lastName, "says, 'Good morning, James.'")
+}
+
+func (sa secretAgent) speak() {
+	fmt.Println(sa.firstName, sa.lastName, "says, 'Shaken not stirred.'")
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
 func main() {
-	fmt.Println()
+	xi := []int{2, 3, 4, 5, 42}
+	fmt.Println(xi)
+
+	m := map[string]int{
+		"Todd":    45,
+		"Matthew": 31,
+	}
+	fmt.Println(m)
+
+	p1 := person{
+		"Miss",
+		"Moneypenny",
+	}
+	fmt.Println(p1)
+
+	p1.speak()
+
+	sa1 := secretAgent{
+		person{
+			"James",
+			"Bond",
+		},
+		true,
+	}
+	sa1.speak()
+
+	sa1.person.speak()
+
+	// Polymorphism
+	saySomething(p1)
+	saySomething(sa1)
 }
